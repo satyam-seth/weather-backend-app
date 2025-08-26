ddfc1b59ad997c1caf9096612d44704f2ca9e183
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-26 22:23:41 +0530

    Add dummy test

--- climate/tests.py
+++ climate/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class DummyTestCase(TestCase):
+
+    def test_dummy(self):
+        """A dummy test that always passes"""
+
+        self.assertEqual(1 + 1, 2)
