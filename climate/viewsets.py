246e00e937b3fd376a6522eaf1e26e42af9e44bc
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-28 21:26:34 +0530

    Add filter in climate record viewset

--- climate/viewsets.py
+++ climate/viewsets.py
@@ -1,11 +1,13 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from .models import ClimateRecord
-from .serializers import ClimateRecordSerializer
+from climate.filters import ClimateRecordFilter
+from climate.models import ClimateRecord
+from climate.serializers import ClimateRecordSerializer
 
 
 class ClimateRecordViewSet(ReadOnlyModelViewSet):  # pylint: disable=too-many-ancestors
     """Climate Record ViewSet"""
 
-    queryset = ClimateRecord.objects.all()
+    queryset = ClimateRecord.objects.all().order_by("year")
     serializer_class = ClimateRecordSerializer
+    filterset_class = ClimateRecordFilter
