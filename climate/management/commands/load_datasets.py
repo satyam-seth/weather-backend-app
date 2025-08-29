ebbd78eb522d9299e66d2c1f6d515f9b3c6ecdd2
Satyam Seth <satyam1998.1998@gmail.com>
2025-08-30 01:08:57 +0530

    Update load datasets command to load data for all regions

--- climate/management/commands/load_datasets.py
+++ climate/management/commands/load_datasets.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from climate.parser import DATASETS, fetch_and_process_climate_data
+from climate.parser import DATASETS, REGIONS, fetch_and_process_climate_data
 
 
 class Command(BaseCommand):
@@ -11,20 +11,24 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         """Handle command to fetch and parse datasets."""
 
-        for dataset, url in DATASETS.items():
-            self.stdout.write(f"Fetching {dataset} from {url}...")
-            try:
-                fetch_and_process_climate_data(url, dataset)
-                self.stdout.write(self.style.SUCCESS(f"Successfully loaded {dataset}."))
-
-            except ValueError as e:
-                self.stdout.write(
-                    self.style.ERROR(f"Failed to load {dataset}: {str(e)}")
-                )
-
-            except ConnectionError as e:
-                self.stdout.write(
-                    self.style.ERROR(
-                        f"Connection error while loading {dataset}: {str(e)}"
+        for region in REGIONS:
+            for dataset, param in DATASETS.items():
+                url = f"https://www.metoffice.gov.uk/pub/data/weather/uk/climate/datasets/{param}/date/{region}.txt"
+                self.stdout.write(f"Fetching {region} - {dataset} from {url}...")
+                try:
+                    fetch_and_process_climate_data(url, region, dataset)
+                    self.stdout.write(
+                        self.style.SUCCESS(f"Successfully loaded {dataset}.")
                     )
-                )
+
+                except ValueError as e:
+                    self.stdout.write(
+                        self.style.ERROR(f"Failed to load {dataset}: {str(e)}")
+                    )
+
+                except ConnectionError as e:
+                    self.stdout.write(
+                        self.style.ERROR(
+                            f"Connection error while loading {dataset}: {str(e)}"
+                        )
+                    )
--- climate/parser/__init__.py
+++ climate/parser/__init__.py
@@ -1,7 +1,8 @@
-from .datasets import DATASETS
+from .datasets import DATASETS, REGIONS
 from .script import fetch_and_process_climate_data
 
 __all__ = [
     "DATASETS",
+    "REGIONS",
     "fetch_and_process_climate_data",
 ]
