3f2f4fe67e9065a3894b5e67a76db253255881fe
Satyam Seth <satyam1998.1998@gmail.com>
2025-10-15 13:04:00 +0530

    Add and apply CachedReadOnlyModelViewSet for viewsets

--- climate/viewsets.py
+++ climate/viewsets.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework.viewsets import ReadOnlyModelViewSet
@@ -19,43 +20,69 @@ from climate.serializers import (
 )
 
 
-class ClimateRegionViewSet(ReadOnlyModelViewSet):
+class CachedReadOnlyModelViewSet(ReadOnlyModelViewSet):
+    """Base ReadOnlyModelViewSet with caching applied to retrieve and list methods."""
+
+    cache_timeout = settings.CACHE_TTL
+
+    def apply_cache_decorator(self):
+        """Applies cache_page decorator to list and retrieve methods."""
+
+        # print("Applying cache decorators", self.cache_timeout)
+
+        method_decorator(
+            cache_page(self.cache_timeout, key_prefix=self.get_cache_key_prefix())
+        )(self.list)
+        method_decorator(
+            cache_page(self.cache_timeout, key_prefix=self.get_cache_key_prefix())
+        )(self.retrieve)
+
+    def get_cache_key_prefix(self):
+        """Override this method in your ViewSet to provide a unique key_prefix."""
+
+        return self.__class__.__name__.lower()
+
+    # To simulate latency for testing caching
+    # def get_queryset(self):
+    #     sleep(2)
+    #     return super().get_queryset()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.apply_cache_decorator()
+
+
+class ClimateRegionViewSet(CachedReadOnlyModelViewSet):
     """Climate Region ViewSet"""
 
     queryset = ClimateRegion.objects.all()
     serializer_class = ClimateRegionSerializer
 
-    @method_decorator(cache_page(60 * 15, key_prefix="product_retrieve"))
-    def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
 
-    @method_decorator(cache_page(60 * 15, key_prefix="product_list"))
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
-
-
-class ClimateParameterViewSet(ReadOnlyModelViewSet):
+class ClimateParameterViewSet(CachedReadOnlyModelViewSet):
     """Climate Parameter ViewSet"""
 
     queryset = ClimateParameter.objects.all()
     serializer_class = ClimateParameterSerializer
 
 
-class ClimateMonthlyViewSet(ReadOnlyModelViewSet):
+class ClimateMonthlyViewSet(CachedReadOnlyModelViewSet):
     """Climate Monthly ViewSet"""
 
     queryset = ClimateMonthly.objects.all()
     serializer_class = ClimateMonthlySerializer
 
 
-class ClimateSeasonalViewSet(ReadOnlyModelViewSet):
+class ClimateSeasonalViewSet(CachedReadOnlyModelViewSet):
     """Climate Seasonal ViewSet"""
 
     queryset = ClimateSeasonal.objects.all()
     serializer_class = ClimateSeasonalSerializer
 
 
-class ClimateRecordViewSet(ReadOnlyModelViewSet):  # pylint: disable=too-many-ancestors
+class ClimateRecordViewSet(
+    CachedReadOnlyModelViewSet
+):  # pylint: disable=too-many-ancestors
     """Climate Record ViewSet"""
 
     queryset = (
