1c7f60c6625a9d6e5b46dc784887aaef3401d6c1
Satyam Seth <satyam1998.1998@gmail.com>
2025-10-16 16:16:16 +0530

    refactor: use model choices from init module for filters

--- climate/filters.py
+++ climate/filters.py
@@ -1,5 +1,6 @@
 import django_filters
 
+from climate import Parameter, Region
 from climate.models import ClimateRecord
 
 
@@ -9,16 +10,14 @@ class ClimateRecordFilter(django_filters.FilterSet):
     # Filter by region name (FK → ClimateRegion.region)
     region = django_filters.ChoiceFilter(
         field_name="region__region",  # follow FK
-        choices=ClimateRecord._meta.get_field("region").related_model.Region.choices,
+        choices=Region.choices,
         lookup_expr="iexact",
     )
 
     # Filter by parameter (FK → ClimateParameter.parameter)
     parameter = django_filters.ChoiceFilter(
         field_name="parameter__parameter",
-        choices=ClimateRecord._meta.get_field(
-            "parameter"
-        ).related_model.Parameter.choices,
+        choices=Parameter.choices,
         lookup_expr="iexact",
     )
 
